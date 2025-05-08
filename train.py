f425bbcef0d02dfb3b79035f5c8606af1313a355
Vladimir Eremeev <nonexistent>
2025-05-08 17:14:03 +0300

    мониторим F1, позитивные классы имеют больший вес

--- train.py
+++ train.py
@@ -6,6 +6,7 @@ from pathlib import Path
 import torch
 from torch.utils.data import DataLoader
 from torch.nn.parallel import DataParallel
+from torchmetrics import F1Score
 
 from transformers import AutoTokenizer
 from omegaconf import OmegaConf
@@ -61,17 +62,21 @@ def main(argv):
     
     model = DataParallel(TextGraphClassifier(cfg.model, freeze_transformer=cfg.model.freeze_transformer).to(device))
     optimizer = torch.optim.Adam(model.parameters(), lr=cfg.train.learning_rate, weight_decay=cfg.train.weight_decay)
-    criterion = torch.nn.BCEWithLogitsLoss()
+    criterion = torch.nn.BCEWithLogitsLoss(pos_weight=torch.tensor([10.0])).to(device)
 
     best_val_loss = float('inf')
+    best_f1 = 0
 
     model_path = Path(cfg.train.save_path)
     model_path.mkdir(parents=True, exist_ok=True)
+
+    f1_calculator = F1Score(task='binary').to(device)
 
     for epoch in trange(cfg.train.epochs):
         model.train()
         total_loss = 0
         train_acc = 0
+
         for batch in tqdm(train_loader, desc='Train'):
             optimizer.zero_grad()
             predictions = model(batch)
@@ -89,6 +94,7 @@ def main(argv):
         with torch.no_grad():
             val_loss = 0
             val_acc = 0
+            val_f1 = 0
             for val_batch in tqdm(val_loader, desc='Val'):
                 predictions = model(val_batch)
                 loss = criterion(predictions, val_batch['label'].float().view(-1, 1).to(device))
@@ -96,18 +102,27 @@ def main(argv):
             
                 acc = compute_accuracy(predictions.cpu(), val_batch['label'].float().view(-1, 1))
                 val_acc += acc
+
+                f1 = f1_calculator(predictions, val_batch['label'].float().view(-1, 1).to(device))
+                val_f1 += f1.item()
     
         avg_val_acc = val_acc / len(val_loader)
         avg_val_loss = val_loss / len(val_loader)    
+        avg_val_f1 = val_f1 / len(val_loader)
 
         print(f'Epoch [{epoch+1}/{cfg.train.epochs}], '
               f'train loss: {total_loss / len(train_loader):.4f}, '
               f'train acc: {avg_train_acc:.4f}, '
-              f'val loss: {val_loss / len(val_loader):.4f}, '
-              f'val acc: {avg_val_acc:.4f}')        
+              f'val loss: {avg_val_loss:.4f}, '
+              f'val acc: {avg_val_acc:.4f}, '
+              f'val f1: {avg_val_f1:.4f}') 
 
-        if avg_val_loss < best_val_loss:
-            best_val_loss = avg_val_loss
+        if avg_val_loss < best_val_loss or avg_val_f1 > best_f1:
+            if avg_val_loss < best_val_loss:
+                best_val_loss = avg_val_loss
+            if avg_val_f1 > best_f1:
+                best_f1 = avg_val_f1
+
             torch.save({
                 'epoch': epoch,
                 'model_state_dict': model.module.state_dict() if isinstance(model, DataParallel) else model.state_dict(),
